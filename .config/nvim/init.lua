5e3d79dd10d670787bec15f63943f8ac207994b7
sumik5 <sumik@legiotech.jp>
2026-01-10 12:30:48 +0900

    feat(nvim): 行番号表示とTelescopeキーマップを追加
    Neovim設定の機能拡張:
      - 行番号表示を有効化（vim.opt.number = true）
      - Telescopeのキーマップを追加:
        - <leader>ff: ファイル検索
        - <leader>fg: live grep（テキスト検索）
        - <leader>fb: バッファ一覧
        - <leader>fh: ヘルプタグ検索

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,9 @@ vim.diagnostic.config()
 -- Leaderキーをスペースに設定
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.opt.number = true
+
+
 
 vim.keymap.set('n', '<Leader>ee', ':NvimTreeToggle<CR>')
 
@@ -13,3 +16,9 @@ require("config.lazy")
 
 require("mason").setup()
 require("mason-lspconfig").setup()
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
